444f27a0f3e768293eaf632ac6918d6f1461b4b8
James Wo <jameswo1234@gmail.com>
2020-12-02 20:00:55 -0500

    model changes

--- app/src/main/java/com/example/photos/Data.java
+++ app/src/main/java/com/example/photos/Data.java
@@ -1,7 +1,28 @@
 package com.example.photos;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Data implements Serializable {
-    Album[] albums;
+    List<Album> albums;
+
+    public Data(){
+        albums = new ArrayList<Album>();
+    }
+
+    /*
+    public static void saveData(String fileName){
+        FileOutputStream fos = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+        ObjectOutputStream os = new ObjectOutputStream(fos);
+        os.writeObject(this);
+        os.close();
+        fos.close();
+    }
+    */
+
 }
+
+
