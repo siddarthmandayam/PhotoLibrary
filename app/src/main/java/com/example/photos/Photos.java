3745faa8e2cbb76b056f5710ed4119892fddb4f1
James Wo <jameswo1234@gmail.com>
2020-12-03 00:39:54 -0500

    serialization with gson is done

--- app/src/main/java/com/example/photos/Photos.java
+++ app/src/main/java/com/example/photos/Photos.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +15,10 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,15 +36,11 @@ public class Photos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //set home_page to be the layout
         setContentView(R.layout.home_page);
-
         //hook up all elements by ther id
         listOfAlbums = findViewById(R.id.album_list);
 
-        //manually initialize for now, will worry about serialization later
-        userAlbums = new ArrayList<Album>();
-        userAlbums.add(new Album("Album 1", null));
-        userAlbums.add(new Album("Album 2", null));
-        userAlbums.add(new Album("Album 3", null));
+        //load data into list
+        loadData();
 
 
 
@@ -90,9 +91,30 @@ public class Photos extends AppCompatActivity {
 
     }
     private void createNewAlbum(String newAlbumName){
-
         userAlbums.add(new Album(newAlbumName, null));
+        saveData();
 
+
+
+    }
+    private void saveData(){
+        SharedPreferences sharedP = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedP.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(userAlbums);
+        editor.putString("data", json);
+        editor.apply();
+    }
+
+    private void loadData(){
+        SharedPreferences sharedP = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedP.getString("data", null);
+        Type type = new TypeToken<List<Album>>(){}.getType();
+        userAlbums = gson.fromJson(json, type);
+
+        if(userAlbums == null)
+            userAlbums = new ArrayList<Album>();
 
     }
     //Pass the selected album information
@@ -101,7 +123,7 @@ public class Photos extends AppCompatActivity {
 
         //add mapping "selected_album" -> userAlbum[position]
         bundle.putSerializable("selected_album", userAlbums.get(position));
-        Intent intent = new Intent(this, Photos.class);
+        Intent intent = new Intent(this, SelectedAlbumView.class);
         intent.putExtras(bundle);
         startActivity(intent);
 
