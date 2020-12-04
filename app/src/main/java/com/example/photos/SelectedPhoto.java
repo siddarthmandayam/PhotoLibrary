5207874cc15b97d35d1a4a86b72a4c66bb0fbed2
Siddarth Mandayam <siddarth.mandayam@gmail.com>
2020-12-04 14:34:03 -0500

    add and delete tag

--- app/src/main/java/com/example/photos/SelectedPhoto.java
+++ app/src/main/java/com/example/photos/SelectedPhoto.java
@@ -1,10 +1,13 @@
 package com.example.photos;
 
 import android.app.AlertDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,7 +19,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class SelectedPhoto extends AppCompatActivity {
 
@@ -32,6 +37,7 @@ public class SelectedPhoto extends AppCompatActivity {
         setContentView(R.layout.activity_selected_photo);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
+        loadData();
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -73,6 +79,9 @@ public class SelectedPhoto extends AppCompatActivity {
 
     public boolean onOptionsItemSelected(MenuItem item){
         switch(item.getItemId()){
+            case R.id.delete_tag_action:
+                promptUserDeleteTag();
+                return true;
             case R.id.add_tag_action:
                 promptUserAddNewTag();
                 return true;
@@ -84,12 +93,100 @@ public class SelectedPhoto extends AppCompatActivity {
         }
     }
 
-    private void promptUserAddNewTag() {
+    public boolean isValidTag(String tag) {
+        return tag.matches("(.*)=(.*)");
+    }
+
+    private void promptUserDeleteTag() { // have to add in error messages still
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         EditText input = new EditText(this);
         input.setHint("Enter tag here ");
 
-        // still have to finish this
+        builder.setView(input);
+        String tag = input.getText().toString();
+
+        // error message here for if tag is not entered in correct format
+
+        String[] tagDeli= tag.split("=");
+        String key = tagDeli[0];
+        String value = tagDeli[1];
+
+        // error message here for if tag entered is not present
+
+        String existingValue = allAlbums.get(albumPosition).photos.get(photoPosition).tags.get(key);
+        String[] existingValueDelimited = existingValue.split(",");
+        StringBuilder newValue = new StringBuilder();
+
+        for (String s : existingValueDelimited) {
+            if (!s.equals(value)) {
+                newValue.append(s + ",");
+            }
+        }
+
+        if (newValue.length() != 0) {
+            newValue.setLength(newValue.length() - 1);
+            allAlbums.get(albumPosition).photos.get(photoPosition).tags.put(key, newValue.toString());
+        }
+        else {
+            allAlbums.get(albumPosition).photos.get(photoPosition).tags.remove(key);
+        }
+
+        TextView selectedPhotoTags = (TextView) findViewById(R.id.selectedPhotoTags);
+        StringBuilder sbTags = new StringBuilder();
+        for(String k: allAlbums.get(albumPosition).photos.get(photoPosition).tags.keySet()) {
+            sbTags.append(k);
+            sbTags.append("=");
+            sbTags.append(allAlbums.get(albumPosition).photos.get(photoPosition).tags.get(k));
+            sbTags.append("\n");
+        }
+        if (sbTags.length() > 0) {
+            sbTags.setLength(sbTags.length() - 1);
+        }
+        selectedPhotoTags.setText(sbTags.toString());
+
+        saveData();
+    }
+
+    private void promptUserAddNewTag() { // have to add in error messages still
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        EditText input = new EditText(this);
+        input.setHint("Enter tag here ");
+
+        builder.setView(input);
+        String tag = input.getText().toString();
+
+        // check here to see if tag is entered in correct format
+
+        String[] tagDelim = tag.split("=");
+        String addedKey = tagDelim[0];
+        String addedValue = tagDelim[1];
+
+        if (!allAlbums.get(albumPosition).photos.get(photoPosition).tags.containsKey(addedKey)) {
+            allAlbums.get(albumPosition).photos.get(photoPosition).tags.put(addedKey, addedValue);
+        }
+        else {
+            String[] existingValueDelimited =
+                    allAlbums.get(albumPosition).photos.get(photoPosition).tags.get(addedKey).split(",");
+            StringBuilder existingValueWithAddedValue =
+                    new StringBuilder(allAlbums.get(albumPosition).photos.get(photoPosition).tags.get(addedKey));
+            existingValueWithAddedValue.append("," + addedValue);
+            allAlbums.get(albumPosition).photos.get(photoPosition).tags.put(addedKey, existingValueWithAddedValue.toString());
+        }
+
+        TextView selectedPhotoTags = (TextView) findViewById(R.id.selectedPhotoTags);
+        StringBuilder sbTags = new StringBuilder();
+        for(String key: allAlbums.get(albumPosition).photos.get(photoPosition).tags.keySet()) {
+            sbTags.append(key);
+            sbTags.append("=");
+            sbTags.append(allAlbums.get(albumPosition).photos.get(photoPosition).tags.get(key));
+            sbTags.append("\n");
+        }
+        if (sbTags.length() > 0) {
+            sbTags.setLength(sbTags.length() - 1);
+        }
+        selectedPhotoTags.setText(sbTags.toString());
+
+        saveData();
     }
 
     private void promptUserDeletePhoto() {
@@ -133,6 +230,7 @@ public class SelectedPhoto extends AppCompatActivity {
 
             // now we delete the photo and update photo position
             allAlbums.get(albumPosition).photos.remove(oldPosition);
+            saveData();
 
         }
 
@@ -165,6 +263,7 @@ public class SelectedPhoto extends AppCompatActivity {
             // now we delete the photo
             allAlbums.get(albumPosition).photos.remove(oldPosition);
             photoPosition = 0;
+            saveData();
         }
 
     }
@@ -222,5 +321,26 @@ public class SelectedPhoto extends AppCompatActivity {
             selectedPhotoTags.setText(sbTags.toString());
 
         }
+    }
+
+    private void saveData(){
+        SharedPreferences sharedP = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedP.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(allAlbums);
+        editor.putString("data", json);
+        editor.apply();
+    }
+
+    private void loadData(){
+        SharedPreferences sharedP = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedP.getString("data", null);
+        Type type = new TypeToken<List<Album>>(){}.getType();
+        allAlbums = gson.fromJson(json, type);
+
+        if(allAlbums == null)
+            allAlbums = new ArrayList<Album>();
+
     }
 }
